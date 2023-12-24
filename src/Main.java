262f0b8d2977f947bfd12a64acd2889d0b7a5cf4
iamfzk <fahadzaafarkhan@gmail.com>
2023-12-24 16:53:06 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //int int2 =2;
+
+      /*  double  kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore > 25){
+            highScore = 1000 + highScore; // and bonus points
+
+        }
+        int health = 100;
+        if ((health < 25) && (highScore>1000)){
+    highScore = highScore - 1000;
+        }*/
+
+
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+
     }
 }
